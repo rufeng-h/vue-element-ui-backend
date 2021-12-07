73329dd4085da7c4ddd7fca7f0979e455f663a3a
rufeng-h <chunfengh@163.com>
2021-12-07 16:40:41 +0800

    商品分类完成

---
+++ src/main/java/com/rufeng/vuemall/validator/group/Delete.java
@@ -0,0 +1,9 @@
+package com.rufeng.vuemall.validator.group;
+/**
+ * @author 黄纯峰
+ * @time 2021-12-07 10:12
+ * @package com.rufeng.vuemall.validator.group
+ * @description TODO
+ */
+public interface Delete {
+}
